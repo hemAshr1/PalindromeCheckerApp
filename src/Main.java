25dbb9bff0eae98306615d022d6c0f9972901431
hemashri2524 <hemashrim25@gmail.com>
2026-03-16 14:42:43 +0530

    UC2:Hardcoded palindrome

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,28 @@
+import java.util.Scanner;
+
 class PalindromeCheckerApp{
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Welcome to the Palindrome Checker Management System");
         System.out.println("Version : 1.0");
+
+        System.out.print("Enter a string: ");
+        String input  = sc.nextLine();
+
+        boolean isPalindrome = true;
+
+        for (int i = 0; i < input.length() / 2; i++) {
+            if(input.charAt(i) != input.charAt(input.length() - 1 - i)){
+                isPalindrome = false;
+                break;
+            }
+        }
+        if (isPalindrome){
+            System.out.println("Is it a Palindrome? : true");
+        }else{
+            System.out.println("Is it a Palindrome? : false");
+        }
+
+        sc.close();
     }
 }
