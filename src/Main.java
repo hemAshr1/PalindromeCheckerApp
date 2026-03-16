25048368c3303852240f579e309dd46afe3e9a23
hemashri2524 <hemashrim25@gmail.com>
2026-03-16 15:09:25 +0530

    UC2:Hardcoded palindromeUC3: Palindrome Check Using String Reverse

--- src/Main.java
+++ src/Main.java
@@ -9,17 +9,16 @@ class PalindromeCheckerApp{
         System.out.print("Enter a string: ");
         String input  = sc.nextLine();
 
-        boolean isPalindrome = true;
+        String reversed = "";
 
-        for (int i = 0; i < input.length() / 2; i++) {
-            if(input.charAt(i) != input.charAt(input.length() - 1 - i)){
-                isPalindrome = false;
-                break;
-            }
+        // Iterate from last character to first
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed = reversed + input.charAt(i);
         }
-        if (isPalindrome){
+
+        if (input.equals(reversed)) {
             System.out.println("Is it a Palindrome? : true");
-        }else{
+        } else {
             System.out.println("Is it a Palindrome? : false");
         }
 
