8e2d62e380c871093e3b331abd71cba424099d8a
hemashri2524 <hemashrim25@gmail.com>
2026-03-16 14:30:17 +0530

    Base application setup

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+class PalindromeCheckerApp{
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Palindrome Checker Management System");
+        System.out.println("Version : 1.0");
+    }
+}
